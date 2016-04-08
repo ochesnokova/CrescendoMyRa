ccd0260e834a8b4e4acd696fab24ac9af152bd1f
ochesnokova <chesnokova.olga@gmail.com>
2016-04-07 20:59:39 -0700

    added string

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,4 +4,6 @@
 public class Test {
 
     String string = "fsgsfdgg";
+
+    Webdriver driver = new FirefoxDriver();
 }
---
+++ src/test/resources/cucumber/LoginTests/Login.feature
@@ -0,0 +1,4 @@
+Feature: Login
+
+  Scenario: sfsdfkjbndfsj
+    Given login to app
